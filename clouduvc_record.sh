2968154df36329433603c1924ced3b082136cca8
Oleg Dolgy <olvyd@yandex.com>
2020-08-07 00:33:32 +0300

    Add FFmpeg support.

--- clouduvc_record.sh
+++ clouduvc_record.sh
@@ -23,12 +23,12 @@ conf_path_remote=mycloud:media/video
 # Retention period in minutes for local storage.
 # Used to delete files that are too old.
 # Set this value to 0 if you don't want to delete files.
-conf_ret_local=$( (60 \* 24 \* 45) )
+conf_ret_local=$(expr 60 \* 24 \* 45)
 # Retention period in minutes for cloud storage.
 # Used to delete files that are too old.
 # Set this value to 0 if you don't want to delete files.
 # You may leave this field blank if conf_use_cloud == 0.
-conf_ret_cloud=$( (60 \* 24 \* 8) )
+conf_ret_cloud=$(expr 60 \* 24 \* 8)
 # Recording start time in format "HH:MM".
 # Videos will be saved from $conf_start_tm to $conf_end_tm.
 # Set $conf_end_tm equal to $conf_start_tm if you want to record videos all day long.
@@ -36,22 +36,34 @@ conf_start_tm="05:00"
 # Recording end time in format "HH:MM"
 conf_end_tm="23:30"
 # Max duration of one video in seconds
-conf_duration=$( (60 \* 10) )
+conf_duration=$(expr 60 \* 10)
 # Camera resolution in format WIDTHxHEIGHT
 conf_res=320x240
-# FPS for guvcview
+# Video FPS
 conf_fps=10
+# FFmpeg use flag.
+# If your camera doesn't support auto brightness or auto contrast, use guvcview.
+conf_use_ffmpeg=0
+# Codec for ffmpeg (see available options for ffmpeg -c:v param)
+conf_codec_ffmpeg=libx264
 # Codec for guvcview (see available options for guvcview --video_codec param)
-conf_codec=mp43
+conf_codec_guvcview=mp43
 # Output video file extension
 conf_ext=avi
 # ************************************
 
 # ********** SCRIPT ******************
 # Check dependencies
-if [[ "$(which guvcview)" == "" ]]; then
-    echo "Please, install guvcview."
-    exit 1
+if [[ conf_use_ffmpeg -ne 0 ]]; then
+    if [[ "$(which ffmpeg)" == "" ]]; then
+        echo "Please, install ffmpeg."
+        exit 1
+    fi
+else
+    if [[ "$(which guvcview)" == "" ]]; then
+        echo "Please, install guvcview."
+        exit 1
+    fi
 fi
 if [[ conf_use_cloud -ne 0 && "$(which rclone)" == "" ]]; then
     echo "Please, install rclone."
@@ -81,9 +93,9 @@ do
         elif [[ ($conf_start_tm < $conf_end_tm && (! $curr_tm < $conf_start_tm && ! $curr_tm > $conf_end_tm)) ||
                 ($conf_start_tm > $conf_end_tm && (! $curr_tm < $conf_start_tm || ! $curr_tm > $conf_end_tm)) ]]; then
         if [[ ! $curr_tm > $conf_end_tm ]]; then
-            rec_duration=$( ($end_tm_sec - "$curr_tm_sec" + 60) )
+            rec_duration=$(expr "$end_tm_sec" - "$curr_tm_sec" + 60)
         else
-            rec_duration=$( (86400 - "$curr_tm_sec" + "$end_tm_sec" + 60) )
+            rec_duration=$(expr 86400 - "$curr_tm_sec" + "$end_tm_sec" + 60)
         fi
         if [[ $rec_duration -gt $conf_duration ]]; then
             rec_duration=$conf_duration
@@ -94,17 +106,30 @@ do
     if [[ rec_duration -gt 0 ]]; then
         file_name=$(date +%Y-%m-%d_%H-%M-%S)
         echo -e "\rStarting a new recording: $file_name ($rec_duration seconds)"
-        guvcview \
-            --video=$conf_path_local/"$file_name".$conf_ext \
-            --video_timer="$rec_duration" \
-            --video_codec=$conf_codec \
-            --resolution=$conf_res \
-            --device=$conf_device \
-            --fps=$conf_fps \
-            --gui=none \
-            --audio=none \
-            --render=none \
-            --exit_on_term
+        full_file_name=$conf_path_local/"$file_name".$conf_ext
+
+        if [[ conf_use_ffmpeg -ne 0 ]]; then
+            ffmpeg -y \
+                -i $conf_device \
+                -t "$rec_duration" \
+                -r $conf_fps \
+                -s $conf_res \
+                -c:v $conf_codec_ffmpeg \
+                -preset ultrafast \
+                "$full_file_name"
+        else
+            guvcview \
+                --video="$full_file_name" \
+                --video_timer="$rec_duration" \
+                --video_codec=$conf_codec_guvcview \
+                --resolution=$conf_res \
+                --device=$conf_device \
+                --fps=$conf_fps \
+                --gui=none \
+                --audio=none \
+                --render=none \
+                --exit_on_term
+        fi
 
         # Run script that copies new video to the cloud storage
         if [[ conf_use_cloud -ne 0 ]]; then
